f6bfa839d6c3638b35f939784b55f21bf3747ef4
Bradley Lewis <32816451+Jblewis9@users.noreply.github.com>
2018-04-22 19:58:00 +0100

    Add files via upload

---
+++ editQuestions.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.awt.*;
+
+public class editQuestions {
+
+
+    public static void main(String[] args) throws IOException{
+        File file = new File("Questionbank.csv");
+        
+        Desktop desktop = Desktop.getDesktop();
+        if(file.exists()) desktop.open(file);
+        
+    }
+
+}
